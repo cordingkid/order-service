b83eb052443b63142dbe71635d0693cd4ecd8712
codingkid <jjg1014@naver.com>
2024-11-05 21:59:55 +0900

    feat: BookClient를 사용하여 Http 통신으로 주문 비즈니스 로직 수정

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.orderservice.order.domain;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -10,6 +12,7 @@ import reactor.core.publisher.Mono;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final BookClient bookClient;
 
     public Flux<Order> getAllOrders(){
         return orderRepository.findAll();
@@ -17,16 +20,33 @@ public class OrderService {
 
     /**
      * 주문 생성
-     * @param isbn
-     * @param quantity
-     * @return
      */
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        return Mono.just(buildRejectedOrder(isbn, quantity))
+        return bookClient.getBookByIsbn(isbn)
+                .map(book -> buildAcceptedOrder(book, quantity))
+                .defaultIfEmpty(
+                        // book 없을경우 주문 거부
+                        buildRejectedOrder(isbn, quantity)
+                )
                 .flatMap(orderRepository::save);
     }
 
-    // 주문 거부
+    /**
+     * 주문 성공
+     */
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.of(
+                book.isbn(),
+                book.title() + " - " + book.author(),
+                book.price(),
+                quantity,
+                OrderStatus.ACCEPTED
+        );
+    }
+
+    /**
+     * 주문 거부
+     */
     public static Order buildRejectedOrder(String bookIsbn, int quantity) {
         return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
     }
