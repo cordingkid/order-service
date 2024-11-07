3744bd3284b55a0149c14f5c5391afda19a10fba
codingkid <jjg1014@naver.com>
2024-11-07 21:48:06 +0900

    test: repository 테스트 코드 추가

--- src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -47,6 +47,14 @@ class OrderRepositoryR2dbcTests {
     }
 
     @Test
+    void findOrderByIdWhenNotExisting() {
+        StepVerifier
+                .create(orderRepository.findById(394L))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
     void createRejectedOrder() {
         var rejectedOrder = OrderService.buildRejectedOrder( "1234567890", 3);
         StepVerifier
