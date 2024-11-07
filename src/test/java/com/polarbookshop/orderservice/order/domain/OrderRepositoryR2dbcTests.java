0382fd2f6f0c6b88a9c817a7e7569dc40ab5de05
codingkid <jjg1014@naver.com>
2024-11-07 21:27:47 +0900

    test: 데이터 R2DBC 슬라이스 통합 테스트

---
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -0,0 +1,57 @@
+package com.polarbookshop.orderservice.order.domain;
+
+import com.polarbookshop.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+@Import(DataConfig.class)   // R2DBC 설정 활성화
+@Testcontainers             // 테스트컨테이너의 자동 시작과 중지 활성화
+class OrderRepositoryR2dbcTests {
+
+    // 테스트를 위한 PostgreSQL 컨테이너 식별
+    @Container
+    static PostgreSQLContainer<?> postgresql =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.12"));
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    // 테스트 PostgreSQL 인스턴스에 연결하도록 R2DBC와 플라이웨이를 설정 변경
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format(
+                "r2dbc:postgresql://%s:%s/%s",
+                postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName()
+        );
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder( "1234567890", 3);
+        StepVerifier
+                .create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
+                .verifyComplete();
+    }
+}
