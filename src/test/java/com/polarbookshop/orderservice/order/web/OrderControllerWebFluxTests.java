63eb895313f52366e1e279b81ab70b6606e25a26
codingkid <jjg1014@naver.com>
2025-01-21 21:51:20 +0900

    test: OAuth2 테스트 코드 작성

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -1,41 +1,43 @@
 package com.polarbookshop.orderservice.order.web;
 
+import com.polarbookshop.orderservice.config.SecurityConfig;
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import reactor.core.publisher.Mono;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 // OrderController를 대상으로 한 웹플러스 컴포넌트에 집중하는 테스트
 @WebFluxTest(OrderController.class)
+@Import(SecurityConfig.class)
 class OrderControllerWebFluxTests {
 
     @Autowired
-    private WebTestClient webClient;
+    WebTestClient webClient;
 
     @MockBean
-    private OrderService orderService;
+    OrderService orderService;
 
     @Test
     void whenBookNotAvailableThenRejectOrder() {
         var orderRequest = new OrderRequest("1234567890", 3);
-        var expectedOrder = OrderService
-                .buildRejectedOrder(orderRequest.isbn(), orderRequest.quantity());
-
-        // OrderService 모의 빈이 어떻게 작동해야하는지 지정
-        BDDMockito
-                .given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+        var expectedOrder = OrderService.buildRejectedOrder(orderRequest.isbn(), orderRequest.quantity());
+        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
                 .willReturn(Mono.just(expectedOrder));
 
         webClient
+                .mutateWith(SecurityMockServerConfigurers.mockJwt()
+                        .authorities(new SimpleGrantedAuthority("ROLE_customer")))
                 .post()
                 .uri("/orders")
                 .bodyValue(orderRequest)
